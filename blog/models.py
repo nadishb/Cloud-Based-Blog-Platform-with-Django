7dfb418906c2540eee5a49bb74d7c83068821ba6
Nadish <123068920+nadishb@users.noreply.github.com>
2025-04-08 11:19:04 +0530

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class BlogPost(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
