1410dfb4b9f1340738d9f32535a6cae8a0bbea7d
Nadish <123068920+nadishb@users.noreply.github.com>
2025-04-08 11:19:54 +0530

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import BlogPost
 
+from .models import Post
 def home(request):
-    posts = BlogPost.objects.all().order_by('-created_at')
+    posts = Post.objects.all().order_by('-created_at')
     return render(request, 'home.html', {'posts': posts})
 
 def post_detail(request, pk):
-    post = get_object_or_404(BlogPost, pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'post_detail.html', {'post': post})
